5ae70e14c91c72f8142f7b9d29ea6948df58ab5d
parsh1002 <parshvajjain1002@gmail.com>
2026-04-16 01:56:21 +0530

    Code changes in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,17 +1,40 @@
 package com.example;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
+        // Open Products Page
+        driver.get("https://automationexercise.com/products");
+        // Maximize Browser Window
         driver.manage().window().maximize();
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+        // Add Product 4 to Cart
+        driver.findElement(By.cssSelector("[data-product-id='4']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector(".btn.btn-success.close-modal.btn-block")).click();
+
+        // Add Product 5 to Cart
+        driver.findElement(By.cssSelector("[data-product-id='5']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector(".btn.btn-success.close-modal.btn-block")).click();
+
+        // Add Product 6 to Cart
+        driver.findElement(By.cssSelector("[data-product-id='6']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector(".btn.btn-success.close-modal.btn-block")).click();
+
+        // Open Cart Page
+        driver.get("https://automationexercise.com/view_cart");
+
+        // Refresh Cart Page
+        driver.navigate().refresh();
+
+        // Wait for 10 seconds
+        Thread.sleep(10000);
+
+        // Close Browser
+        driver.quit();
     }
 }
